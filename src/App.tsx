01ad460463ff2a703f0fa889e93b309991d8f488
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-26 14:55:01 +0600

    Create Plans and Notes page, implement router in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
 import AppShell from './components/layout/AppShell'
+import { Routes, Route, Navigate } from 'react-router'
 import TasksPage from './pages/Tasks/Tasks'
+import PlansPage from './pages/Plans/Plans'
+import NotesPage from './pages/Notes/Notes'
 
 function App() {
-
   return (
     <>
       <AppShell>
-        <TasksPage />
+        <Routes>
+          <Route path="/" element={<Navigate to="/tasks" replace />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/plans" element={<PlansPage />} />
+          <Route path="/notes" element={<NotesPage />} />
+        </Routes>
       </AppShell>
     </>
   )
--- src/pages/Plans/Plans.tsx
+++ src/pages/Plans/Plans.tsx
@@ -1,4 +1,4 @@
-export default function TasksPage(){
+export default function PlansPage(){
     return (
         <>
             
