db4c66fd7b3da2d1e31729b7e41fefbed300dafd
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-27 18:09:23 +0600

    Create SignIn page, add link in Sidebar button, update the routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import { Routes, Route, Navigate } from 'react-router'
 import TasksPage from './pages/Tasks/Tasks'
 import PlansPage from './pages/Plans/Plans'
 import NotesPage from './pages/Notes/Notes'
+import SignInPage from './pages/SignIn/SignIn'
 
 function App() {
   return (
     <>
-      <AppShell>
-        <Routes>
-          <Route path="/" element={<Navigate to="/tasks" replace />} />
-          <Route path="/tasks" element={<TasksPage />} />
-          <Route path="/plans" element={<PlansPage />} />
-          <Route path="/notes" element={<NotesPage />} />
-        </Routes>
-      </AppShell>
+      <Routes>
+        <Route path="/signin" element={<SignInPage />} />
+        <Route
+          path="/*"
+          element={
+            <AppShell>
+              <Routes>
+                <Route path="/" element={<Navigate to="/tasks" replace />} />
+                <Route path="/tasks" element={<TasksPage />} />
+                <Route path="/plans" element={<PlansPage />} />
+                <Route path="/notes" element={<NotesPage />} />
+              </Routes>
+            </AppShell>
+          }
+        />
+      </Routes>
     </>
   )
 }
--- src/components/layout/SidebarLoginButton.tsx
+++ src/components/layout/SidebarLoginButton.tsx
@@ -1,11 +1,14 @@
 import { LogIn } from "lucide-react";
 import { Button } from "../ui/button";
+import { Link } from "react-router";
 
 export default function SidebarLoginButton() {
     return (
-        <Button className="bg-(--accent-soft) text-(--accent) font-medium text-md py-5">
-            <LogIn className="w-4 h-4" />
-            Sign In
-        </Button>
+        <Link to={"/signin"} className="">
+            <Button className="w-full bg-(--accent-soft) text-(--accent) font-medium text-md py-5">
+                <LogIn className="w-4 h-4" />
+                Sign In
+            </Button>
+        </Link>
     )
 }
