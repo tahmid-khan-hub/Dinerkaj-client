89ce93786ea651b3dfd5b4c9ee46e25138bbafb1
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-27 17:55:24 +0600

    Update the design of SidebarLogin button

--- src/components/layout/SidebarLoginButton.tsx
+++ src/components/layout/SidebarLoginButton.tsx
@@ -1,7 +1,11 @@
+import { LogIn } from "lucide-react";
 import { Button } from "../ui/button";
 
 export default function SidebarLoginButton() {
     return (
-        <Button className="bg-(--accent-soft) text-(--accent) font-medium text-md py-5">Sign In</Button>
+        <Button className="bg-(--accent-soft) text-(--accent) font-medium text-md py-5">
+            <LogIn className="w-4 h-4" />
+            Sign In
+        </Button>
     )
 }
