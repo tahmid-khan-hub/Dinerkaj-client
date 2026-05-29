9ad68736aaf5a127833943bbca1a689b3a49b32e
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-29 14:23:26 +0600

    Structure and design the SignIn page content and fields

--- src/pages/SignIn/components/SignInContent.tsx
+++ src/pages/SignIn/components/SignInContent.tsx
@@ -5,23 +5,24 @@ import { motion } from "framer-motion";
 export default function SignInContent() {
     const navigate = useNavigate();
   return (
-    <div className="w-full var(--bg) min-h-screen flex items-center justify-center px-4">
-      <motion.div>
+    <div className="w-full bg-(--bg) min-h-screen flex items-center justify-center px-4">
+      <motion.div
+      initial={{ opacity: 0, x: 30 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6 }}
+      className="w-full max-w-md bg-(--bg-surface) border border-(--border) p-8 space-y-6 rounded-xl">
         {/* Title */}
         <div className="text-center pt-2 space-y-1">
-          <h2 className="text-3xl font-bold"> Welcome Back </h2>
-          <p className="text-sm" > Sign in to continue with Dinerkaj </p>
+          <h2 className="font-serif text-(--text-primary) font(--font-display) text-3xl font-bold"> Welcome Back </h2>
+          <p className="text-sm text-(--text-secondary)" > Sign in to continue with Dinerkaj </p>
         </div>
         {/* Divider */}
-        <div className="flex items-center gap-3">
-          <div className="flex-1 h-px" style={{ background: "var(--border)" }} />
-          <span
-            className="text-sm"
-            style={{ color: "var(--text-muted)", fontFamily: "var(--font-ui)" }}
-          >
+        <div className="flex items-center gap-5">
+          <div className="flex-1 h-px bg-(--border)" />
+          <span className="text-sm text-(--text-muted)">
             one click sign in
           </span>
-          <div className="flex-1 h-px" style={{ background: "var(--border)" }} />
+          <div className="flex-1 h-px bg-(--border)" />
         </div>
         {/* Form */}
         <SignInFormFields
--- src/pages/SignIn/components/SignInFormFields.tsx
+++ src/pages/SignIn/components/SignInFormFields.tsx
@@ -13,7 +13,7 @@ export default function SignInFormFields() {
         <div>
             <Button
             disabled={loading}
-            className="w-full flex items-center justify-center gap-3 py-3 font-semibold text-base transition-all hover:opacity-85 disabled:opacity-50">
+            className="w-full flex items-center justify-center gap-3 py-5 font-semibold text-base text-(--accent) bg-(--accent-soft) rounded-md transition-all disabled:opacity-50">
                 {/* Google SVG icon */}
                 <svg width="18" height="18" viewBox="0 0 48 48">
                     <path fill="#FFC107" d="M43.6 20H24v8h11.3C33.6 33.1 29.3 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3 0 5.8 1.1 7.9 3l5.7-5.7C34.1 6.5 29.3 4 24 4 12.9 4 4 12.9 4 24s8.9 20 20 20c11 0 19.7-8 19.7-20 0-1.3-.1-2.7-.1-4z"/>
