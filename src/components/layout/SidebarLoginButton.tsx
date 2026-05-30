7389966d26176e5c49dabaddf7cfbe1a9d504cf9
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-30 16:28:36 +0600

    Add the SignOut option in SidebarLogin button component

--- src/components/layout/SidebarLoginButton.tsx
+++ src/components/layout/SidebarLoginButton.tsx
@@ -1,14 +1,34 @@
-import { LogIn } from "lucide-react";
+import { LogIn, LogOut } from "lucide-react";
 import { Button } from "../ui/button";
 import { Link } from "react-router";
+import { useAuth } from "@/hooks/useAuth";
+import { authClient } from "@/lib/authClient";
 
 export default function SidebarLoginButton() {
+    const { isLoggedIn, isPending } = useAuth();
+
+    const handleSignOut = async() => {
+        await authClient.signOut()
+    }
+
+    if(isPending) return <p>loading...</p>
     return (
-        <Link to={"/signin"} className="">
-            <Button className="w-full bg-(--accent-soft) text-(--accent) font-medium text-md py-5">
-                <LogIn className="w-4 h-4" />
-                Sign In
-            </Button>
-        </Link>
+        <>
+            {!isLoggedIn ? (
+                <Link to={"/signin"} className="">
+                    <Button className="w-full bg-(--accent-soft) text-(--accent) font-medium text-md py-5">
+                        <LogIn className="w-4 h-4" />
+                        Sign In
+                    </Button>
+                </Link>
+            ) : (
+                <Button
+                onClick={handleSignOut}
+                className="w-full bg-(--accent-soft) text-(--accent) font-medium text-md py-5">
+                    <LogOut className="w-4 h-4" />
+                    Sign Out
+                </Button>
+            )}
+        </>
     )
 }
