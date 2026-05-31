aa9aaa4ceda0997df9f45b6cc436a38ced88a47a
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-31 17:02:58 +0600

    Create a New Task button in Tasks page heading

--- src/pages/Tasks/components/TasksPageHeading.tsx
+++ src/pages/Tasks/components/TasksPageHeading.tsx
@@ -1,15 +1,23 @@
+import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth"
+import { Plus } from "lucide-react";
 
 export default function TasksPageHeading() {
     const { user } = useAuth();
     const firstName = user?.name?.split(" ")[0];
 
     return (
-        <div className="flex items-start justify-between gap-4">
+        <div className="flex items-start justify-between gap-4 p-3">
             <div className="flex flex-col gap-1">
-                <h1 className="text-3xl md:text-4xl text-(--text-primary) p-3">
+                <h1 className="text-3xl md:text-4xl text-(--text-primary)">
                     Welcome back, <span className="font-serif text-(--accent)">{firstName}</span>.
                 </h1>
+            </div>
+            <div>
+                <Button className="bg-(--accent-soft) text-(--accent) p-4">
+                    <Plus />
+                    New Task
+                </Button>
             </div>
         </div>
     )
