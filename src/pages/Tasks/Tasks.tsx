d9215574b420e64e0dde9781a006c3fa994d44b0
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-31 17:27:19 +0600

    Add task fetching using TanStack Query

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,8 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import SignInSuccessAlert from "../SignIn/components/SignInSuccessAlert";
 import TaskItem from "./components/TaskItem";
 import TasksPageHeading from "./components/TasksPageHeading";
+import type { Task } from "./lib/types";
 
 export default function TasksPage(){
+    const { data: tasks=[], isLoading } = useQuery<Task[]>({
+        queryKey: ['tasks'],
+        queryFn: async () => {
+            const res = await fetch("http://localhost:3000/tasks", { credentials: "include" })
+            if (!res.ok) throw new Error("Failed to fetch tasks");
+            return res.json();
+        }
+    })
+
+    const pendingTasks = tasks.filter(task => task.status === 'pending');
     return (
         <>
             <SignInSuccessAlert />
