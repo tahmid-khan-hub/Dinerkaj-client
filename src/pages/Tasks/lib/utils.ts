44ae19b4d21c699a7c669de5a45fac26b96fef9d
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-06-01 19:18:03 +0600

    Create utils for tasks page

---
+++ src/pages/Tasks/lib/utils.ts
@@ -0,0 +1,7 @@
+import type { Priority } from "./types";
+
+export const priorities: { value: Priority; label: string }[] = [
+  { value: "low", label: "Low" },
+  { value: "medium", label: "Medium" },
+  { value: "high", label: "High" },
+];
