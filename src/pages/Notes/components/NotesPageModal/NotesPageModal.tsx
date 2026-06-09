74453a23b02842d86be2bc5fff84a6b547e1581d
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-06-09 20:33:14 +0600

    Update and restructure the NotesPageModal component

--- src/pages/Notes/components/NotesPageModal/NotesPageModal.tsx
+++ src/pages/Notes/components/NotesPageModal/NotesPageModal.tsx
@@ -1,5 +1,8 @@
 import { Dialog } from "@/components/ui/dialog";
 import NotesPageModalContent from "./NotesPageModalContent";
+import type { NotesForm } from "../../lib/types";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface NotesPageModalProps {
     open: boolean;
@@ -7,11 +10,59 @@ interface NotesPageModalProps {
 }
 
 export default function NotesPageModal({ open, onClose }:NotesPageModalProps) {
-    return (
-        <>
-            <Dialog open={open}>
-              <NotesPageModalContent />
-            </Dialog>
-        </>
-    )
+  const queryClient = useQueryClient();
+  const NotesInitialForm: NotesForm = {
+    notesTitle: "",
+    category: "personal",
+    notesDescription: "",
+  }
+
+  const [notesFormData, setNotesFormData] = useState<NotesForm>(NotesInitialForm);
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async(data: NotesForm) => {
+      const res = await fetch(`http://localhost:3000/api/notes`, {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+      if(!res.ok) throw new Error("Failed to create new note");
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] })
+      CloseNotesModal()
+    },
+    onError: () => {  }
+  })
+
+  const updateNotesForm = (field: string, value: string) => {
+    setNotesFormData(prev => ({...prev, [field]: value}))
+  }
+
+  const CloseNotesModal = () => {
+    setNotesFormData(NotesInitialForm);
+    onClose();
+  }
+
+  const submitNote = () => {
+    if(!notesFormData.notesTitle.trim()) return;
+    mutate({...notesFormData, 
+      notesTitle: notesFormData.notesTitle.trim(),
+      notesDescription: notesFormData.notesDescription.trim(),
+    })
+  }
+  return (
+    <>
+      <Dialog open={open} onOpenChange={CloseNotesModal}>
+        <NotesPageModalContent
+        formData={notesFormData}
+        updateForm={updateNotesForm}
+        handleClose={CloseNotesModal}
+        isPending={isPending}
+        onSubmit={submitNote} />
+      </Dialog>
+    </>
+  )
 }
